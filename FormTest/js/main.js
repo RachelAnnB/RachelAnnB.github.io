61456a70017da84cc4b9b6ccf637da122248ebb6
Rachel Ann Broering <rachelannbroering@gmail.com>
2015-08-21 15:15:13 -0400

    facebook login test1

--- FormTest/js/main.js
+++ FormTest/js/main.js
@@ -3,5 +3,19 @@ $(function() {
   
   Parse.initialize("vNGHN5aZTRjQxsuFV4cX4ZTIVCFHRlCJKQm0O6Qy", "JzDD3z7R0H7JsLezVM70t9rMnETN3YxAtQczlMEi");
   
+  //------
+  //Models
+  //------
   
+  //-----------
+  //Collections
+  //-----------
+  
+  //-----
+  //Views
+  //-----
+  
+  //-------
+  //Routers
+  //-------
 });
