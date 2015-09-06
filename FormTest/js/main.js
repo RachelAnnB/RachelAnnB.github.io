074961d62ff294cfeb3ea6819113ee047b66cf4b
Rachel Ann Broering <rachelannbroering@gmail.com>
2015-09-05 22:05:51 -0400

    pull multiple objects test 11

--- FormTest/js/main.js
+++ FormTest/js/main.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
           var phone = results[i].get("phone");
           var email = results[i].get("email");
           
-          var nameString = name1 + name2 + name3 + name4 + name5 + name6 + name7 + name8 + name9 + name10 " \n";
+          var nameString = name1 + name2 + name3 + name4 + name5 + name6 + name7 + name8 + name9 + name10 + " ";
           
           
         }
