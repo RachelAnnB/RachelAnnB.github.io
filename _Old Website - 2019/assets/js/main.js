9412198d1495f30e59ea0a1d6969eb398865b664
Rachel Broering <RachelAnnB@users.noreply.github.com>
2023-10-06 11:13:54 -0400

    Removed old files
    Deleted files for a past version of the site

--- _Old Website - 2019/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(window).on('scroll',function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 100) {
-        $(".navbar-default").addClass("navbarScroll");
-    } else {
-        $(".navbar-default").removeClass("navbarScroll");
-    }
-});
